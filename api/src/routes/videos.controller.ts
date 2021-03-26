10cd6e4bc84dfa08fc151398573f02ab1de99580
Maikell <maikell.ods10@gmail.com>
2021-03-25 22:31:31 -0400

    Backend Finished

--- api/src/routes/videos.controller.ts
+++ api/src/routes/videos.controller.ts
@@ -1,21 +1,43 @@
 import { RequestHandler } from "express";
+import Video from "./Video";
 
-export const createVideo: RequestHandler = (req, res) => {
-    res.json("creating videos");
+export const createVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findOne({ url: req.body.url });
+    if (videoFound)
+        return res.status(301).json({ message: "The url already exists" });
+
+    const video = new Video(req.body);
+    const savedVideo = await video.save();
+    res.json(savedVideo);
 };
 
-export const getVideos: RequestHandler = (req, res) => {
-    res.json("getting videos");
+export const getVideos: RequestHandler = async (req, res) => {
+    try {
+        const videos = await Video.find();
+        return res.json(videos);
+    } catch (e) {
+        res.json(e);
+    }
 };
 
-export const getVideo: RequestHandler = (req, res) => {
-    res.json("getting only one video");
+export const getVideo: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findById(req.params.id);
+    if (!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 };
 
-export const deleteVideos: RequestHandler = (req, res) => {
-    res.json("deleting a video");
+export const deleteVideos: RequestHandler = async (req, res) => {
+    const videoFound = await Video.findByIdAndDelete(req.params.id);
+    if (!videoFound) return res.status(204).json();
+    return res.json(videoFound);
 };
 
-export const updateVideo: RequestHandler = (req, res) => {
-    res.json("updating a video");
+export const updateVideo: RequestHandler = async (req, res) => {
+    const videoUpdated = await Video.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    if (!videoUpdated) return res.status(204).json();
+    res.json(videoUpdated)
 };
